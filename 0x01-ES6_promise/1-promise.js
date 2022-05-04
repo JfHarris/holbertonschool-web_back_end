a77484b2630d8b7c4efd83aba6a3b5927f61b28d
jfharris <jfharris1983@gmail.com>
2022-05-04 00:37:37 -0500

    Maybe?

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,8 @@
-getFullResponseFromAPI(success)
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    reject(Error('The fake API is not working currently'));
+  });
+}
