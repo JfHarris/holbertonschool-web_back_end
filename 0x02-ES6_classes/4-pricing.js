9183104beec81bf3f6ee35d367f991848c93485b
jfharris <jfharris1983@gmail.com>
2022-05-06 18:30:27 -0500

    Added static lines

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,4 +3,16 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
+  get amount() { return this._amount; }
+  set amount(amount) { this._amount = amount; }
+  get currency() { return this._currency; }
+  set currency(currency) {this._currency = currency; }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
