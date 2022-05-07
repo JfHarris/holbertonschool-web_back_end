b6fd50d20e228838f2186e452a3bdda763fa6bd3
jfharris <jfharris1983@gmail.com>
2022-05-06 23:44:55 -0500

    Still not matching example output

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,4 +7,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLen) {
+    if (typeof newLen !== 'number') throw TypeError('Length must be a number');
+    this._length = newLen;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStuds) {
+    if (newStuds.constructor !== Array && newStuds.every((entry) => typeof entry === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStuds;
+  }
 }
